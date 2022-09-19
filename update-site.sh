4dc632a9d3701e895a42b18d73ca6ad618ea8463
Bandla <rbandla@albany.edu>
2022-09-19 14:49:11 -0400

    update site

--- update-site.sh
+++ update-site.sh
@@ -1,17 +1,17 @@
-#!/bin/bash
-# cd /network/rit/lab/researchdiv/sdat/Matchmaker/js
-# # /network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node createData.js
-# /network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node createDict.js
-# /network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node createIdArray.js
-# /network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node PI_Sponsor_mapping.js
-# /network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node Keywords.js
-# /network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node treeCreator.js
-# /network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node Keywords.js
+# !/bin/bash
+cd /network/rit/lab/researchdiv/sdat/Matchmaker/js
+/network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node createData.js
+/network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node createDict.js
+/network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node createIdArray.js
+/network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node PI_Sponsor_mapping.js
+/network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node Keywords.js
+/network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node treeCreator.js
+/network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node Keywords.js
 
-# cd /network/rit/lab/researchdiv/sdat/Matchmaker/JSONs
-# /network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node newJson.js
+cd /network/rit/lab/researchdiv/sdat/Matchmaker/JSONs
+/network/rit/lab/researchdiv/bin/node-v12.18.3/bin/node newJson.js
 
-# cd ..
+cd ..
 
 git add -A
 git pull
